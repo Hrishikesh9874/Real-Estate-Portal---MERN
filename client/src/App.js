664e188cfe1900c4bd61ccb4e33a144ecd0f808f
Hrishikesh <hrishikeshanand3@gmail.com>
2024-12-14 15:32:39 +0530

    Installed React.js@18.3.1 with tailwind css, created initial pages and routes

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
+        <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/profile' element={<Profile></Profile>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
