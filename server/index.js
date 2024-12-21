caffab16597109a235de5de5709842244dd75d62
Hrishikesh <hrishikeshanand3@gmail.com>
2024-12-21 21:50:29 +0530

    Completed SignUp page functionality

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const { mongoose } = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user.route');
 const authRouter = require('./routes/auth.route');
+const cors = require('cors');
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 dotenv.config();
 
