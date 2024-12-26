0e5282bb51bb7a482cf404f51d9f335eaba05ea0
Hrishikesh <hrishikeshanand3@gmail.com>
2024-12-26 21:11:33 +0530

    Created and added OAuth Component

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7ytIAEq43w7qOP_habGXcuOM4-__Pm30",
+  authDomain: "mern-estate-662e1.firebaseapp.com",
+  projectId: "mern-estate-662e1",
+  storageBucket: "mern-estate-662e1.firebasestorage.app",
+  messagingSenderId: "648776767056",
+  appId: "1:648776767056:web:6a0e9980e3ab6768212355"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux';
 import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice.js';
+import OAuth from "../components/OAuth.js";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -69,6 +70,7 @@ export default function SignIn() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
+        <OAuth></OAuth>
       </form>
 
       <div className="flex gap-2 mt-2">
