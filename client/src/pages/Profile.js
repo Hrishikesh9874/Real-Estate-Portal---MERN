cde2f6cca722c141a53166e1b1fc837fdfb3e83c
Hrishikesh <hrishikeshanand3@gmail.com>
2024-12-31 21:39:51 +0530

    Completed Image Upload Functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,63 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux"
 
 export default function Profile() {
 
   const {currentUser} = useSelector((state)=> state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [profileImage, setProfileImage] = useState(currentUser.avatar);
+  const [progress, setProgress] = useState(0);
+  const [uploadError, setUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+
+
+
+  function handleFileUpload(file) {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "Real-Estate");
+    data.append("cloud_name", "dbzrbmfi8");
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "https://api.cloudinary.com/v1_1/dbzrbmfi8/image/upload");
+
+    xhr.upload.addEventListener("progress", (event) => {
+      if (event.lengthComputable) {
+        const percentComplete = Math.round((event.loaded / event.total) * 100);
+        setProgress(percentComplete);
+      }
+    });
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        setProfileImage(response.url);
+        setFormData({...formData, avatar: response.url});
+        setUploadError(null);
+      } else {
+        setUploadError("Upload failed with status: " + xhr.status);
+      }
+      setProgress(0);
+    };
+
+    xhr.onerror = () => {
+      setUploadError("An error occurred during the upload.");
+      setProgress(0);
+    };
+
+    xhr.send(data);
+    
+  }
+
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  }, [file])
+
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -10,7 +65,19 @@ export default function Profile() {
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
 
       <form className='flex flex-col gap-2'>
-        <img src={currentUser.avatar} alt="Profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+        <input onChange={(e)=> setFile(e.target.files[0])} type="file" ref={fileRef} accept="image/*" hidden/>
+        <img onClick={()=> fileRef.current.click()} src={profileImage} alt="Profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+        <p className="text-sm self-center">
+          {
+            uploadError ? (
+              <span className="text-red-700">Error Image Upload</span>
+            ) : progress > 0 && progress < 100 ? (
+              <span className="text-slate-700">{`Uploading ${progress}%`}</span>
+            ) : progress === 100 ? (
+              <span className="text-green-700">Image Successfully uploaded!</span>
+            ) : ''
+          }
+        </p>
         <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username"/>
         <input type="text" placeholder="email" className="border p-3 rounded-lg" id="email"/>
         <input type="text" placeholder="password" className="border p-3 rounded-lg" id="password"/>
