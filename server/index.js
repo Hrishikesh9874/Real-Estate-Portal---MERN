284b71f9cdd54c63268eb44bfb39872bd7e01ef9
Hrishikesh <hrishikeshanand3@gmail.com>
2025-01-18 21:03:57 +0530

    Changes for deploy

--- server/index.js
+++ server/index.js
@@ -39,10 +39,10 @@ app.use((err, req, res, next)=> {
     })
 })
 
+const PORT = process.env.PORT || 4000;
 
 
 
-
-app.listen(4000, ()=>{
-    console.log('Server is running on localhost: 4000');
+app.listen(PORT, ()=>{
+    console.log(`Server is running on localhost: ${PORT}`);
 })
